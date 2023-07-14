387420bb8906c23ceb78c410521db766f3491e39
Joshua Khan <Khanjo@gmail.com>
2023-07-14 09:04:09 -0700

    first test constructor for bread class works because of implicit constructor

---
+++ Bakery/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+using Bakery.UserInterfaceModels;
+
+namespace Bakery
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
