b1dce9b3677434e3d16408600130c60328ca257e
Joshua Khan <Khanjo@gmail.com>
2023-07-14 15:47:13 -0700

    update UI, properly adds totals

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,8 +11,8 @@ namespace Bakery
         {
             Console.WriteLine(WelcomeBanner.Welcome);
             Console.WriteLine("Hello!");
-            int discountBreadPrice = 0;
-            int discountPastryPrice = 0;
+            int discountBreadPrice = Bread();
+            int discountPastryPrice = Pastry();
             int total = discountBreadPrice + discountPastryPrice;
             Console.WriteLine("The total for your order will be:");
             Console.WriteLine("$" + total);
@@ -20,39 +20,44 @@ namespace Bakery
         }
         static int Bread()
         {
-            Console.WriteLine("Please enter how many loaves of bread you would like, they are $5 each and we currently have a buy 2 get 1 free deal going on!");
+            Console.WriteLine("Bread loaves $5 each and we currently have a buy 2 get 1 free deal going on!");
+            Console.WriteLine("Please enter how many loaves of bread you would like.");
             string input = Console.ReadLine();
             if (Int32.TryParse(input, out int breadCount))
             {
                 Bread breadOrder = new Bread(breadCount);
                 int breadPrice = breadOrder.BreadCost();
                 int discountBreadPrice = breadOrder.BreadDiscount(breadPrice);
+                Console.WriteLine("The total for your bread will be:");
+                Console.WriteLine("$" + discountBreadPrice);
+                Console.Read();
                 return discountBreadPrice;
             }
             else
             {
                 Console.WriteLine("Please enter a number");
-                Bread();
-                return 0;
+                return Bread();
             }
         }
         static int Pastry()
         {
-            Console.WriteLine("Please enter how many pastries you would like, they are $2 each and we currently have a buy 3 get 1 free deal going on!");
+            Console.WriteLine("Pastries are $2 each and we currently have a buy 3 get 1 free deal going on!");
+            Console.WriteLine("Please enter how many pastries you would like.");
             string input = Console.ReadLine();
             if (Int32.TryParse(input, out int pastryCount))
             {
                 Pastry pastryOrder = new Pastry(pastryCount);
                 int pastryPrice = pastryOrder.PastryCost();
                 int discountPastryPrice = pastryOrder.PastryDiscount(pastryPrice);
+                Console.WriteLine("The total for your pastries will be:");
+                Console.WriteLine("$" + discountPastryPrice);
+                Console.Read();
                 return discountPastryPrice;
             }
             else
             {
                 Console.WriteLine("Please enter a number");
-                Pastry();
-                return 0;
-
+                return Pastry();
             }
         }
     }
